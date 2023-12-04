3886af170dc6e0a0bb9d5094010f7f4b105c47bf
nguyensongnghiem <nguyensongnghiem@gmail.com>
2023-12-04 19:36:09 +0700

    SS5

---
+++ src/Session05/Student.java
@@ -0,0 +1,23 @@
+package Session05;
+
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+    public Student () {
+
+    }
+    public void setName(String name) {
+        this.name = name;
+
+    }
+     protected void setClasses (String classes) {
+        this.classes = classes;
+    }
+}
+class TestStudent {
+    public static void main(String[] args) {
+       Student std = new Student();
+       std.setName("Bob");
+       std.setClasses("C10");
+    }
+}
